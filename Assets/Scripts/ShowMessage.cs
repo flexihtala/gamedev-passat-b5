2f275d89e0fd09a161cbf642750f4588c6c31703
flexihtala <flexihtala@gmail.com>
2025-04-26 20:49:15 +0500

    Fix

--- Assets/Scripts/ShowMessage.cs
+++ Assets/Scripts/ShowMessage.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PhoneTrigger : MonoBehaviour
@@ -5,11 +6,17 @@ public class PhoneTrigger : MonoBehaviour
     public PhoneController phoneController;
     public string messageText = "Новое сообщение!";
 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other);
-        if (!other.CompareTag("AddNewMassage")) return;
-        phoneController.ShowMessage(messageText);
-        Destroy(gameObject); // Если надо, чтобы триггер исчезал после активации
+        Debug.Log("a");
     }
+
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     Debug.Log(other);
+    //     if (!other.CompareTag("AddNewMassage")) return;
+    //     phoneController.ShowMessage(messageText);
+    //     Destroy(gameObject); // Если надо, чтобы триггер исчезал после активации
+    // }
 }
