ab7635d611a925f3d3c3ddb5977cfc1601d61f47
flexihtala <flexihtala@gmail.com>
2025-04-27 01:35:02 +0500

    ostanovka

--- Assets/Scripts/WifeTrigger.cs
+++ Assets/Scripts/WifeTrigger.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class WifeTrigger : MonoBehaviour
 {
     public GameObject dogMoment;
+    public GameObject ostanovkaTrigger;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         dogMoment.SetActive(true);
+        ostanovkaTrigger.SetActive(true);
     }
 }
